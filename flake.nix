608e3ed1068907e60435fec4eba39d79768f4c8e
madosuki <yurikasamaaikatsu@gmail.com>
2026-07-09 16:42:40 +0900

    init

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "Development shell for Common Lisp CFFI bindings to libmpv";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs systems (
+          system:
+          f (import nixpkgs { inherit system; })
+        );
+    in
+    {
+      devShells = forAllSystems (
+        pkgs:
+        let
+          libPath = pkgs.lib.makeLibraryPath [
+            pkgs.mpv
+          ];
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.roswell
+              pkgs.sbcl
+              pkgs.sbclPackages.cffi
+              pkgs.mpv
+              pkgs.pkg-config
+            ];
+
+            shellHook = ''
+              export LD_LIBRARY_PATH="${libPath}:''${LD_LIBRARY_PATH:-}"
+              export DYLD_LIBRARY_PATH="${libPath}:''${DYLD_LIBRARY_PATH:-}"
+              export CL_SOURCE_REGISTRY="$PWD//:''${CL_SOURCE_REGISTRY:-}"
+
+              echo "Common Lisp dev shell: roswell, SBCL, CFFI, libmpv"
+            '';
+          };
+        }
+      );
+    };
+}
